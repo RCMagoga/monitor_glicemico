0fbb69d6019681bbee7fe72f5fb53efb36a7a066
Renan Magoga <rcmagoga@gmail.com>
2025-01-04 14:41:32 -0300

    Adicionado classe coleta

---
+++ lib/models/coleta.dart
@@ -0,0 +1,12 @@
+class Coleta {
+  int? id;
+  DateTime data;
+  int jejum;
+  int almoco;
+  int jantar;
+  String obsJejum;
+  String obsAlmoco;
+  String obsJantar;
+
+  Coleta(this.data, this.jejum, this.almoco, this.jantar, this.obsJejum, this.obsAlmoco, this.obsJantar, {this.id});
+}
